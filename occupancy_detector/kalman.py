9a837f85f5efb005149bef1930d1cf394d689f7b
richapat4 <richapat4@gmail.com>
2023-05-18 21:40:50 +1000

    Basic example of kalman filter taking input data and updating it's predictions

--- occupancy_detector/kalman.py
+++ occupancy_detector/kalman.py
@@ -1,15 +1,84 @@
-from filterpy import KalmanFilter 
+# from filterpy import KalmanFilter 
+# import numpy as np
+# from filterpy.common import Q_discrete_white_noise
+
+
 import numpy as np
+from filterpy.kalman import KalmanFilter
+import matplotlib.pyplot as plt
 
-f = KalmanFilter(dim_x = 2, dim_z = 1)
+f = KalmanFilter(6, 3) 
+
+f.x = np.array([0., 0., 0., 0., 0., 0.]) # STATE Vector x, y, z, 
+
+# state matrix based on equations of motion for x,y,z
+
+# A = np.array([[1, 0, 0, dt, 0, 0],
+#               [0, 1, 0, 0, dt, 0],
+#               [0, 0, 1, 0, 0, dt],
+#               [0, 0, 0, 1, 0, 0],
+#               [0, 0, 0, 0, 1, 0],
+#               [0, 0, 0, 0, 0, 1]])
+
+f.F = np.asarray(
+    [
+        [1., 0., 0., 1., 0., 0.],
+        [0., 1., 0., 0., 1., 0.],
+        [0., 0., 1., 0., 0., 1.],
+        [0., 0., 0., 1., 0., 0.],
+        [0., 0., 0., 0., 1., 0.],
+        [0., 0., 0., 0., 0., 1.]
+    ]
+)
+
+#x,y,z position outputs
+f.H = np.array([
+    [1., 0., 0., 0., 0., 0.],
+    [0., 1., 0., 0., 0., 0.],
+    [0., 0., 1., 0., 0., 0.]
+])
+
+f.R = 5 * np.eye(3)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+scatter = ax.scatter([0], [0], [0], marker='o',label='data')
+scatter_2 = ax.scatter([0], [0], [0], marker='o',label='data')
+
+ax.set_xlabel('X')
+ax.set_xlim([-10,10])
+ax.set_ylabel('Y')
+ax.set_ylim([-1,1])
+ax.set_zlabel('Z')
+ax.set_zlim([-1,1])
+
+ax.set_title('3D Scatter Plot')
+ax.legend()
 
 
-#initial array  x position and x velocity 
-f.x = np.array([2.0, 0.0])
+def main():
+    for i in range(10):
+        ax.cla()
+        z = np.array([i, 0.1*i, 0.01*i])
+        print('Measured: ', z)
+        ax.scatter(z[0],z[1],z[2],c='red', marker='o',label='measured')
+        f.predict()
+        print('predicted: ', f.x)
+        ax.scatter(f.x[0],f.x[1],f.x[2],c='blue', marker='o',label='predicted')
+        f.update(z)
+        
+        ax.set_xlabel('X')
+        ax.set_xlim([-10,10])
+        ax.set_ylabel('Y')
+        ax.set_ylim([-1,1])
+        ax.set_zlabel('Z')
+        ax.set_zlim([-1,1])
 
-#state transition matrix 
+        ax.legend()
 
-f.F = np.array([[1.,1.],
-                [0.,1.]])
+        plt.pause(1)
 
-f.H = np.array([[1.,0.]])  #measurement function 
+    plt.show()
+
+if __name__ == '__main__':
+    main()
